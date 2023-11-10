f7bff968d2304f3b371d53ed0687d5ec4f569a46
Md Jawed <mdjawedh22@gmail.com>
2023-11-10 17:43:32 +0530

    Some change in search and other file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { MensComponent } from './mens/mens.component';
 import { WomenComponent } from './women/women.component';
 import { KidsComponent } from './kids/kids.component';
-
 import { CartComponent } from './cart/cart.component';
 import { DetailsComponent } from './details/details.component';
 import { CheckoutComponent } from './checkout/checkout.component';
@@ -14,22 +13,35 @@ import { SellerComponent } from './seller/seller.component';
 import { SellerHomeComponent } from './seller-home/seller-home.component';
 import { SellerAddProductComponent } from './seller-add-product/seller-add-product.component';
 import { SellerUpdateProductComponent } from './seller-update-product/seller-update-product.component';
+import { authGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'men', component: MensComponent },
   { path: 'women', component: WomenComponent },
   { path: 'kids', component: KidsComponent },
-  { path: 'cart', component: CartComponent },
+  { path: 'cart', component: CartComponent, canActivate: [authGuard] },
   { path: 'product/:id', component: DetailsComponent },
   { path: 'user', component: UserComponent },
-  { path: 'checkout', component: CheckoutComponent },
+  { path: 'checkout', component: CheckoutComponent, canActivate: [authGuard] },
   { path: 'search/:query', component: SearchComponent },
   { path: 'seller', component: SellerComponent },
-  { path: 'seller-home', component: SellerHomeComponent },
-  { path: 'seller-add-product', component:SellerAddProductComponent  },
-  { path: 'seller-update-product/:id', component: SellerUpdateProductComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {
+    path: 'seller-home',
+    component: SellerHomeComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'seller-add-product',
+    component: SellerAddProductComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'seller-update-product/:id',
+    component: SellerUpdateProductComponent,
+    canActivate: [authGuard],
+  },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: '**', redirectTo: '/' },
 ];
 
